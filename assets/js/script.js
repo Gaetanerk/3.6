340f2c6d703ba328716526c9a66baebb027f1ade
Gaetanerk <gaetan.turben@outlook.com>
2022-04-25 13:56:20 +0200

    ajout 2eme façon de faire le script

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,19 +3,25 @@ const checkBtn = document.querySelector("#checkBtn");
 
 //tell the grade from the age
 checkBtn.addEventListener("click", () => {
-  if (age.value < 6) {
-    alert("trop jeune");
-  }
-  if (age.value >= 6 && age.value <= 7) {
-    alert("poussin");
-  }
-  if (age.value >= 8 && age.value <= 9) {
-    alert("pupille");
-  }
-  if (age.value >= 10 && age.value <= 11) {
-    alert("minime");
-  }
-  if (age.value >= 12) {
-    alert("cadet");
-  }
+  //   if (age.value < 6) {
+  //     alert("trop jeune");
+  //   }
+  //   if (age.value >= 6 && age.value <= 7) {
+  //     alert("poussin");
+  //   }
+  //   if (age.value >= 8 && age.value <= 9) {
+  //     alert("pupille");
+  //   }
+  //   if (age.value >= 10 && age.value <= 11) {
+  //     alert("minime");
+  //   }
+  //   if (age.value >= 12) {
+  //     alert("cadet");
+  //   }
+  // });
+  if (age.value < 6) "trop jeune";
+  if (age.value >= 6 && age.value <= 7) alert("poussin");
+  if (age.value >= 8 && age.value <= 9) alert("pupille");
+  if (age.value >= 10 && age.value <= 11) alert("minime");
+  if (age.value >= 12) alert("cadet");
 });
